1750e12b98f30069c77f82b0bd1d413e0afd523e
smart <smart@smhrd>
2024-03-15 11:54:01 +0900

    test

--- projectTavern/src/main/java/projectTavern/gyuhyeon.java
+++ projectTavern/src/main/java/projectTavern/gyuhyeon.java
@@ -4,6 +4,15 @@ public class gyuhyeon {
 
 	public static void main(String[] args) {
 		System.out.println("test");
+		
+		int a = 5;
+		int sum;
+		for(int i =0;i<=a;i++) {
+			sum = i+a;
+			System.out.println(sum);
+			
+			
+		}
 
 	}
 
