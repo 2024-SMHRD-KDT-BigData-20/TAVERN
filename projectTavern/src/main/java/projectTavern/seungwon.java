0003a12b6eaeb49ca7834041a917e2e96ea6d9e9
smart <smart@smhrd>
2024-03-15 11:59:16 +0900

    승원실험

---
+++ projectTavern/src/main/java/projectTavern/seungwon.java
@@ -0,0 +1,15 @@
+package projectTavern;
+import java.util.*;
+public class seungwon {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Word");
+		Scanner sc = new Scanner(System.in);
+		
+		int j = 0;
+		int i = sc.nextInt();
+		System.out.println(i);
+		
+	}
+
+}
