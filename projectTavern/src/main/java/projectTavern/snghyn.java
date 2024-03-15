40b4f11a6c66410d9757a2aa269117b467b27c4f
박승현 <meduk1203@gmail.com>
2024-03-15 11:52:24 +0900

    ㅁ

--- projectTavern/src/main/java/projectTavern/snghyn.java
+++ projectTavern/src/main/java/projectTavern/snghyn.java
@@ -1,9 +1,19 @@
 package projectTavern;
 
+import java.util.Scanner;
+
 public class snghyn {
 
 	public static void main(String[] args) {
 		System.out.println("집 보내줘요");
+		Scanner sc = new Scanner(System.in);
+		int i = sc.nextInt();
+		if(i > 10) {
+			System.out.println("집가기");
+		}
+		else {
+			System.out.println("집가기");
+		}
 
 	}
 
