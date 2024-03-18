9b88137af51692e65572b8bf18a32c16a77c139d
박승현 <meduk1203@gmail.com>
2024-03-18 10:31:38 +0900

    AA

--- projectTavern/src/main/webapp/join.jsp
+++ projectTavern/src/main/webapp/join.jsp
@@ -1,8 +1,8 @@
 <%@ page language="java" contentType="text/html; charset=EUC-KR" pageEncoding="EUC-KR"%>
 <!DOCTYPE html>
-<html>
+<html l>
 <head>
-<meta charset="EUC-KR">
+<meta charset="utf-8">
 <title>Insert title here</title>
 <link rel="stylesheet" href="css/join.css">
 </head>
@@ -10,34 +10,36 @@
 	<div id="container">
 		<form action="JoinCon" method="post">
 			<!-- 회원정보 입력 타이틀 -->
-			<div class=tilte>회원정보</div>
+			<div class="title">회원정보</div>
 			<!-- 회원 아이디 입력 -->
 			<div>
 				<div class="text_box">
 					<input type="text" id="user_id" name="id" placeholder="아이디">
-					<label class="form_lavel">아이디</label>
+					<label class="form_label">아이디</label>
 				</div>
-				<div class="error_box"></div>
+				<div class="error_box" id="id_error"></div>
 			</div>
 			<!-- 비밀번호 입력 -->
 			<div>
 				<div class="text_box">
-					<input type="password" id="user_re_pw" name="pw" placeholder="비밀번호 확인">
-					<label class="form_lavel">비밀번호</label>
+					<input type="password" id="user_pw" name="pw" placeholder="비밀번호">
+					<label class="form_label">비밀번호</label>
 				</div>
-				<div class="pw error_box">6글자 이상 입력해주세요</div>
+				<div class="pw error_box" id="pw_error">6글자 이상 입력해주세요</div>
 			</div>
+			<!-- 비밀번호 확인 -->
 			<div>
 				<div class="text_box">
-					<input type="password" id="user_re pw" name="pw" placeholder="비밀번호 확인">
-					<label class="form_lavel">비밀번호 재입력</label>
+					<input type="password" id="user_re_pw" name="re_pw" placeholder="비밀번호 확인">
+					<label class="form_label">비밀번호 재입력</label>
 				</div>
-				<div class="re_pw error_box">비밀번호가 일치하지 않습니다</div>
+				<div class="re_pw error_box" id="re_pw_error">비밀번호가 일치하지 않습니다</div>
 			</div>
+			<!-- 이름 입력 -->
 			<div>
 				<div class="text_box">
 					<input type="text" id="user_name" name="name" placeholder="이름을 입력하세요">
-					<label class="form_lavel">이름</label>
+					<label class="form_label">이름</label>
 				</div>
 			</div>
 			<div>
--- projectTavern/src/main/webapp/js/pw_re.js
+++ projectTavern/src/main/webapp/js/pw_re.js
@@ -1,49 +1,38 @@
-// 비밀번호 불러오기
-let p1 = document.getElementById('user_pw');
-// 비밀번호 재 확인 불러오기'
-let p2 = document.querySelector('#user_re_pw');
-// 비밀번호 error 메시지 불러오기
-let p_e = document.querySelector('.pw');
-// 비밀번호 재 확인 error 메시지 불러오기
-let r_p_e = document.querySelector('.re_pw');
+document.addEventListener('DOMContentLoaded', function() {
+    // 비밀번호 불러오기
+    let p1 = document.getElementById('user_pw');
+    // 비밀번호 재 확인 불러오기
+    let p2 = document.getElementById('user_re_pw');
+    // 비밀번호 error 메시지 불러오기
+    let p_e = document.getElementById('pw_error');
+    // 비밀번호 재 확인 error 메시지 불러오기
+    let r_p_e = document.getElementById('re_pw_error');
 
-// 비밀번호 : 6글자 이상 입력
-function pw(str) {
-	return /^.{6,}$/.test(str);
-}
-// 비밀번호 확인
-function repw(pw1, pw2) {
-	return pw1 === pw2;
-}
+    // 비밀번호 : 6글자 이상 입력
+    function pw(str) {
+        return str.length >= 6;
+    }
 
-// 키보드가 눌렀다 떼어 졌을 때 onkeyup
-p1.onkeyup = function() {
-	if (p1.value.length !== 0) {
-		if (repw(p1.value, p2.value)) {
-			p_e.classList.add('error_box'); // 실패 메시지 가려지기
-		}
-		else {
-			p_e.classList.remove('error_box');
-		}
-	}
-	// 모든 메시지 가리기
-	else {
-		p_e.classList.add('error_box');
-	}
-};
+    // 비밀번호 재확인 일치 확인
+    function repw(pw1, pw2) {
+        return pw1 === pw2;
+    }
 
-// 키보드가 눌렀다 떼어 졌을 때 onkeyup
-p2.onkeyup = function() {
-	if (p2.value.length !== 0) {
-		if (repw(p1.value, p2.value)) {
-			p_e.classList.add('error_box'); // 실패 메시지 가려지기
-		}
-		else {
-			p_e.classList.remove('error_box');
-		}
-	}
-	// 모든 메시지 가리기
-	else {
-		p_e.classList.add('error_box');
-	}
-};
+    // 비밀번호 입력 시 이벤트 처리
+    p1.addEventListener('keyup', function() {
+        if (pw(p1.value)) {
+            p_e.style.display = 'none';
+        } else {
+            p_e.style.display = 'block';
+        }
+    });
+
+    // 비밀번호 재확인 입력 시 이벤트 처리
+    p2.addEventListener('keyup', function() {
+        if (repw(p1.value, p2.value)) {
+            r_p_e.style.display = 'none';
+        } else {
+            r_p_e.style.display = 'block';
+        }
+    });
+});
