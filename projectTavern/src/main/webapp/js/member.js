0cb34e4cc8d1ccbfaed2efd3868880af59e8ab67
smart <smart@smhrd>
2024-03-21 15:49:08 +0900

    memberjs수정

--- projectTavern/src/main/webapp/js/member.js
+++ projectTavern/src/main/webapp/js/member.js
@@ -1,9 +1,4 @@
 function idCheck() {
-        if (document.frm.id.value == "") {
-            alert('아이디를 입력하여 주십시오.');
-            document.frm.id.focus();
-            return;
-        }
         let url = "idcheck.jsp?id=" + document.frm.id.value;
         window.open(url, "_blank_1", "toolbar=no, menubar=no, scrollbars=yes, resizable=no, width=450, height=200");
     }
