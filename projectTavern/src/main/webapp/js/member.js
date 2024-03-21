2074f6893719d7e5dfeb0d224424cc49596fa1b9
smart <smart@smhrd>
2024-03-21 15:35:19 +0900

    idcheck서블릿,JSP,JS구현

--- projectTavern/src/main/webapp/js/member.js
+++ projectTavern/src/main/webapp/js/member.js
@@ -1,10 +1,9 @@
 function idCheck() {
-	if (document.frm.id.value == "") {
-		alert('아이디를 입력하여 주십시오.');
-		document.frm.id.focus();
-		return;
-	}
-	var url = "idCheck.do?id=" + document.frm.id.value;
-	window.open(url, "_blank_1",
-					"toolbar=no, menubar=no, scrollbars=yes, resizable=no, width=450, height=200");
-}
+        if (document.frm.id.value == "") {
+            alert('아이디를 입력하여 주십시오.');
+            document.frm.id.focus();
+            return;
+        }
+        var url = "idcheck.jsp?id=" + document.frm.id.value;
+        window.open(url, "_blank_1", "toolbar=no, menubar=no, scrollbars=yes, resizable=no, width=450, height=200");
+    }
