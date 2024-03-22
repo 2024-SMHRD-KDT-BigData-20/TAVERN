fbd46749f6fda435804df230c921acee38758520
smart <smart@smhrd>
2024-03-22 10:50:00 +0900

    memberjs joinus 실험

--- projectTavern/src/main/webapp/js/member.js
+++ projectTavern/src/main/webapp/js/member.js
@@ -1,4 +1,7 @@
-function idCheck() {
-        let url = "idcheck.jsp?id=" + document.frm.id.value;
-        window.open(url, "_blank_1", "toolbar=no, menubar=no, scrollbars=yes, resizable=no, width=450, height=200");
+// 아이디 중복 유무 ㄱ머사 함수
+function idCheck(frm) {
+        //아이디 입력 확인
+        if(frm.id.value==""){
+			alert("아이디 입력 후 중복확인 버튼을 눌z")
+		}
     }
