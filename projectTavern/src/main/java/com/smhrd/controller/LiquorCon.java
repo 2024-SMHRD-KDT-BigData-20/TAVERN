42e70a59b3c96383027e4522db4cc36a86810752
smart <smart@smhrd>
2024-03-27 16:17:45 +0900

    a

--- projectTavern/src/main/java/com/smhrd/controller/LiquorCon.java
+++ projectTavern/src/main/java/com/smhrd/controller/LiquorCon.java
@@ -16,16 +16,18 @@ public class LiquorCon extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	System.out.println("[LiquorCon]");
+        System.out.println("[LiquorCon]");
+        String liq_type = request.getParameter("liq_type"); // request 파라미터로부터 liq_type 전달 받음
+
         // 술 이름을 가져오기 위한 DAO 객체 생성
         LiquorDAO liquorDAO = new LiquorDAO();
-        // 술 이름 목록 가져오기 (여기서는 위스키만 가져오도록 했습니다)
-        List<LiquorVO> whiskeyList = liquorDAO.liqName("위스키");
-        System.out.println("위스키리스트"+whiskeyList);
+        // 술 이름 목록 가져오기 (liq_type에 따라 다른 술의 목록을 가져오도록 함)
+        List<LiquorVO> liquorList = liquorDAO.liqName(liq_type);
+        System.out.println(liq_type + " 리스트: " + liquorList);
         // 세션에 술 이름 목록을 저장
-        if (whiskeyList != null) {
-        	HttpSession session = request.getSession();
-        	session.setAttribute("whiskeyList", whiskeyList);
+        if (liquorList != null) {
+            HttpSession session = request.getSession();
+            session.setAttribute("liquorList", liquorList);
         }
         // 리다이렉션: 새로운 URL로 클라이언트에게 이동 요청
         response.sendRedirect("liquor_list.jsp");
